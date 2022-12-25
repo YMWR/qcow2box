6be96a81a0ffd9fa81fa87d28220616e92e2e926
Daniel T. Lee <danieltimlee@gmail.com>
2022-12-25 17:27:37 +0900

    qemu: specify qemu binary per architecture
    Signed-off-by: Daniel T. Lee <danieltimlee@gmail.com>

--- 30_provision.sh
+++ 30_provision.sh
@@ -7,4 +7,36 @@ fi
 ARCH=$1
 IMG=$2
 ISO=${3:-ciiso.iso}
-qemu-system-${ARCH} -m 2G -smp 2 -hda $IMG -cdrom $ISO -nic none -nographic
+
+case "${ARCH}" in
+x86_64)
+	QEMU_BINARY=qemu-system-x86_64
+	QEMU_FLAGS=(-smp 2)
+	;;
+arm64)
+	QEMU_BINARY=qemu-system-aarch64
+	QEMU_FLAGS=(-smp 2 -M virt)
+	;;
+armhf)
+	QEMU_BINARY=qemu-system-arm
+	QEMU_FLAGS=(-smp 2 -M virt)
+	;;
+s390x)
+	QEMU_BINARY=qemu-system-s390x
+    QEMU_FLAGS=(-smp 2)
+	;;
+ppc64el)
+	QEMU_BINARY=qemu-system-ppc64le
+    QEMU_FLAGS=(-smp 2)
+	;;
+riscv64)
+	QEMU_BINARY=qemu-system-riscv64
+	QEMU_FLAGS=(-smp 1)
+	;;
+*)
+	echo "Unsupported architecture"
+	exit 1
+	;;
+esac
+
+$QEMU_BINARY "${QEMU_FLAGS[@]}" -m 2G -hda $IMG -cdrom $ISO -nic none -nographic
