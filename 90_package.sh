a50c4595f3396b68fbbb114f40ba306ae0362c51
Daniel T. Lee <danieltimlee@gmail.com>
2022-12-25 15:33:09 +0900

    vagrant: add script for packaging box
    Signed-off-by: Daniel T. Lee <danieltimlee@gmail.com>

---
+++ 90_package.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+if [ $# -eq 0  ]; then
+	echo "no arguments"
+	exit 1
+fi
+
+BOX_NAME=$1
+IMG=$2
+
+cat >metadata.json <<EOF
+{"provider": "libvirt", "format": "qcow2", "virtual_size": 40}
+EOF
+
+cat >Vagrantfile <<EOF
+Vagrant.configure('2') do |config|
+        config.vm.provider :libvirt do |libvirt|
+                libvirt.driver = 'qemu'
+                libvirt.username = 'root'
+                libvirt.connect_via_ssh = false
+                libvirt.storage_pool_name = 'default'
+        end
+end
+EOF
+
+tar -cvzf $BOX_NAME metadata.json Vagrantfile $IMG
