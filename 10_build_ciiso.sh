52b07f8d88ba289cfeacbaebec4447cc1f53b1d0
Daniel T. Lee <danieltimlee@gmail.com>
2022-12-25 15:31:17 +0900

    CI: use cloud-init to provision vagrant configuration
    Signed-off-by: Daniel T. Lee <danieltimlee@gmail.com>

---
+++ 10_build_ciiso.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+if [ $# -eq 0  ]; then
+	echo "no arguments"
+	exit 1
+fi
+
+
+ISO=${1:-ciiso.iso}
+rm -rf $ISO meta-data user-data
+
+cat >meta-data <<EOF
+instance-id: vagrant
+local-hostname: localhost
+EOF
+
+cat >user-data <<EOF
+#cloud-config
+system_info:
+  default_user:
+    name: vagrant
+    sudo: ['ALL=(ALL) NOPASSWD:ALL']
+    groups: sudo
+    ssh-authorized-keys:
+      - ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key
+ssh_pwauth: True
+password: vagrant
+chpasswd: {expire: False}
+write_files:
+  - path: /etc/cloud/cloud-init.disabled
+runcmd:
+  - grubby --update-kernel ALL --args selinux=0 || true
+  - sed -i 's/enforcing/disabled/g' /etc/selinux/config || true
+  - sed -i 's/enforcing/disabled/g' /etc/sysconfig/selinux || true
+  - sed -i 's/.*UseDNS.*/UseDNS no/' /etc/ssh/sshd_config || true
+power_state:
+    delay: now
+    mode: poweroff
+    message: Cloud Init-Done Powering off
+    timeout: 0
+EOF
+
+genisoimage -output $ISO -volid cidata -joliet -rock user-data meta-data
