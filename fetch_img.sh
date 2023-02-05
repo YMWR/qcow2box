7cf652518df1eeec84e49a5dd916947075345f11
Daniel T. Lee <danieltimlee@gmail.com>
2023-02-05 22:37:12 +0900

    Update fetch_img.sh

--- fetch_img.sh
+++ fetch_img.sh
@@ -8,7 +8,7 @@ DIST=$1
 DIST=${DIST,,}  # Lowercase
 ARCH=$2         # options: [arm64, s390x, ppc64el]
 
-URL=https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-$ARCH.img
+URL=http://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-$ARCH.img
 
 if [[ "$DIST" == *"fedora"* ]];then
     PARSE_IMAGE() {
@@ -18,17 +18,17 @@ if [[ "$DIST" == *"fedora"* ]];then
     case "${ARCH}" in
     arm64)
         ARCH=aarch64
-        BASE_URL=https://download.fedoraproject.org/pub/fedora/linux/releases/37/Cloud/$ARCH/images
+        BASE_URL=http://download.fedoraproject.org/pub/fedora/linux/releases/37/Cloud/$ARCH/images
         URL=${BASE_URL}/$(PARSE_IMAGE $BASE_URL)
         ;;
     s390x)
         ARCH=s390x
-        BASE_URL=https://download.fedoraproject.org/pub/fedora-secondary/releases/37/Cloud/$ARCH/images
+        BASE_URL=http://download.fedoraproject.org/pub/fedora-secondary/releases/37/Cloud/$ARCH/images
         URL=${BASE_URL}/$(PARSE_IMAGE $BASE_URL)
         ;;
     ppc64el)
         ARCH=ppc64le
-        BASE_URL=https://download.fedoraproject.org/pub/fedora-secondary/releases/37/Cloud/$ARCH/images
+        BASE_URL=http://download.fedoraproject.org/pub/fedora-secondary/releases/37/Cloud/$ARCH/images
         URL=${BASE_URL}/$(PARSE_IMAGE $BASE_URL)
         ;;
     *)
